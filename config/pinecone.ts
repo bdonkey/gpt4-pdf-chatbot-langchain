220e1aaedc7cc67718a3c6ec3972bb6f6749f49a
bdonkey <scott.schmidt1001@gmail.com>
2023-05-06 09:01:04 -0700

    scott tweaked setup to run

--- config/pinecone.ts
+++ config/pinecone.ts
@@ -2,13 +2,8 @@
  * Change the namespace to the namespace on Pinecone you'd like to store your embeddings.
  */
 
-const PINECONE_INDEX_NAME = 'ss';
-if (!process.env.PINECONE_INDEX_NAME) {
-  throw new Error('Missing Pinecone index name in .env file');
-}
-
 const PINECONE_INDEX_NAME = process.env.PINECONE_INDEX_NAME ?? '';
 
-const PINECONE_NAME_SPACE = 'pdf-test'; //namespace is optional for your vectors
+const PINECONE_NAME_SPACE = 'cython'; //namespace is optional for your vectors
 
 export { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE };
---
+++ ssReadme.md
@@ -0,0 +1,4 @@
+# 2023-05-06
+
+- runs
+- but changes made will conflict with future pulls from upstread due to setup changes.
